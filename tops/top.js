d052c75316d2160f993ae73feb883f168f3909d1
info-estagiarios <114098437+info-estagiarios@users.noreply.github.com>
2022-10-06 16:28:34 -0300

    Update top.js

--- tops/top.js
+++ tops/top.js
@@ -1,12 +1,21 @@
 var item1 = document.querySelector('.geral')
 var item2 = document.querySelector('.agenda')
+var item3 = document.querySelector('.perfil')
 
 function geral(){
     item1.style.display = "flex"
+    item3.style.display = "none"
     item2.style.display = "none"
 }
 
 function agenda(){
     item1.style.display = "none"
+    item3.style.display = "none"
     item2.style.display = "flex"
+}
+
+function perfil(){
+    item1.style.display = "none"
+    item2.style.display = "none"
+    item3.style.display = "flex"
 }
