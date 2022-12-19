69c06c139bd6c44c822c2a9be6390bb55e54cd2c
info-estagiarios <114098437+info-estagiarios@users.noreply.github.com>
2022-12-19 09:13:09 -0300

    Update top.js

--- tops/top.js
+++ tops/top.js
@@ -1,21 +1,77 @@
 var item1 = document.querySelector('.geral')
 var item2 = document.querySelector('.agenda')
 var item3 = document.querySelector('.perfil')
+var item4 = document.querySelector('.colegiados')
+var item5 = document.querySelector('.calendario')
+var item6 = document.querySelector('.download')
+var item7 = document.querySelector('.media')
 
 function geral(){
     item1.style.display = "flex"
     item3.style.display = "none"
     item2.style.display = "none"
+    item5.style.display = "none"
+    item4.style.display = "none"
+    item7.style.display = "none"
+    item6.style.display = "none"
 }
 
 function agenda(){
     item1.style.display = "none"
     item3.style.display = "none"
+    item4.style.display = "none"
+    item5.style.display = "none"
+    item6.style.display = "none"
+    item7.style.display = "none"
     item2.style.display = "flex"
 }
 
 function perfil(){
     item1.style.display = "none"
     item2.style.display = "none"
+    item4.style.display = "none"
+    item5.style.display = "none"
+    item6.style.display = "none"
+    item7.style.display = "none"
     item3.style.display = "flex"
+}
+
+function colegiados(){
+    item1.style.display = "none"
+    item2.style.display = "none"
+    item3.style.display = "none"
+    item5.style.display = "none"
+    item6.style.display = "none"
+    item7.style.display = "none"
+    item4.style.display = "flex"
+}
+
+function calendario(){
+    item1.style.display = "none"
+    item2.style.display = "none"
+    item3.style.display = "none"
+    item4.style.display = "none"
+    item6.style.display = "none"
+    item7.style.display = "none"
+    item5.style.display = "flex"
+}
+
+function download(){
+    item1.style.display = "none"
+    item2.style.display = "none"
+    item3.style.display = "none"
+    item4.style.display = "none"
+    item5.style.display = "none"
+    item7.style.display = "none"
+    item6.style.display = "flex"
+}
+
+function media(){
+    item1.style.display = "none"
+    item2.style.display = "none"
+    item3.style.display = "none"
+    item4.style.display = "none"
+    item5.style.display = "none"
+    item6.style.display = "none"
+    item7.style.display = "flex"
 }
